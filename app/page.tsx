b5729d2dadcf67e3044e590bb8aa4d51653963a2
Robbie <robbiethewadley@gmail.com>
2025-05-15 18:10:29 -0600

    simpler reproduction

--- app/page.tsx
+++ app/page.tsx
@@ -1,118 +1,31 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+/** Add your relevant code here for the issue to reproduce */
 /**
  * generate 1024KB of random text
  */
 function randomText() {
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	const possible =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  return Array.from({ length: 1024 ** 2 }, () =>
-    possible.charAt(Math.floor(Math.random() * possible.length))
-  ).join("");
+	return Array.from({ length: 1024 ** 2 }, () =>
+		possible.charAt(Math.floor(Math.random() * possible.length)),
+	).join("");
 }
 
 export const metadata = {
-  title: "This will not be visible if shared via Slack",
-  description: "This will also not be visible if shared via Slack",
+	title: "This will not be visible if shared via Slack, X, or Facebook",
+	description:
+		"This will also not be visible if shared via Slack, X, or Facebook",
 };
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <style href="large-css" precedence="abc">{`
-          .${randomText()} {
-            color: red;
-          }
-        `}</style>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<>
+			hello world
+			<style href="large-css" precedence="abc">{`
+        .${randomText()} {
+          color: red;
+        }
+      `}</style>
+		</>
+	);
 }
