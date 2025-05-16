e67270f118d4d16f7dfc06991edd8898f9c5a743
Robbie <robbiethewadley@gmail.com>
2025-05-15 18:11:19 -0600

    re-add images

--- app/page.tsx
+++ app/page.tsx
@@ -3,29 +3,29 @@
  * generate 1024KB of random text
  */
 function randomText() {
-	const possible =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	return Array.from({ length: 1024 ** 2 }, () =>
-		possible.charAt(Math.floor(Math.random() * possible.length)),
-	).join("");
+  return Array.from({ length: 1024 ** 2 }, () =>
+    possible.charAt(Math.floor(Math.random() * possible.length))
+  ).join("");
 }
 
 export const metadata = {
-	title: "This will not be visible if shared via Slack, X, or Facebook",
-	description:
-		"This will also not be visible if shared via Slack, X, or Facebook",
+  title: "This will not be visible if shared via Slack, X, or Facebook",
+  description:
+    "This will also not be visible if shared via Slack, X, or Facebook",
 };
 
 export default function Home() {
-	return (
-		<>
-			hello world
-			<style href="large-css" precedence="abc">{`
+  return (
+    <>
+      hello world
+      <style href="large-css" precedence="abc">{`
         .${randomText()} {
           color: red;
         }
       `}</style>
-		</>
-	);
+    </>
+  );
 }
