bff140ce7a6bfa73095566de7a20e6a7c9d7dc6a
Robbie <robbiethewadley@gmail.com>
2025-05-15 18:12:07 -0600

    update paths

---
+++ app/works/page.tsx
@@ -0,0 +1,10 @@
+/** Add your relevant code here for the issue to reproduce */
+
+export const metadata = {
+	title: "This will be visible if shared via Slack, X, or Facebook",
+	description: "This will also be visible if shared via Slack, X, or Facebook",
+};
+
+export default function Home() {
+	return <>hello world</>;
+}
